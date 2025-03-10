503536f662ec814294308cd4c793820d83faa244
Sreevasan Siva <s.sreevasan@gmail.com>
2025-03-09 23:09:24 -0500

    End of session 3

--- driver.py
+++ driver.py
@@ -5,6 +5,50 @@ from subprocess import Popen, PIPE
 
 mem = Popen(['python', 'mem.py'], stdout=PIPE, stdin=PIPE, encoding='utf8')
 
+def main():
+    if len(sys.argv) != 2:
+        print("Please upload the log file")
+        sys.exit(1)
+
+    log_file = sys.argv[1]
+
+    loggerProcess = subprocess.Popen(["python3", "logger.py", log_file], stdin=subprocess.PIPE, text=True)
+    encryptionProcess = subprocess.Popen(["python3", "encryption.py"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+    history = [] #Stores messages
+
+    def log()
+        loggerProcess.stdin.write()
+    log("START Logging Started")
+
+
+
+while True:
+    print("\nMenu:")
+    print("1. Password")
+    print("2. Encrypt")
+    print("3. Decrypt")
+    print("4. History")
+    print("5. Quit")
+
+    choice = input("Enter command choice: ").strip().lower()
+
+    if choice == "password":
+        message = input("Use a string in (1)history or (2) new string: ")
+
+        if message == "2":
+            password = input("Enter a password: ")
+            
+        elif message == "1":
+            if history:
+                password = search_history(password)
+
+
+    elif choice == "encrypt":
+    elif choice == "decrypt":
+    elif choice == "history"
+    elif choice == "quit":
+        break
+
 
 for i in range(10,0,-1):
     mem.stdin.write("write\n")
@@ -19,3 +63,11 @@ mem.stdin.write("halt\n")
 mem.stdin.flush()
 
 mem.wait()
+
+logger_process.wait()
+encryption_process.wait()
+
+if __name__ == "__main__":
+    main()
+
+    
