c6aeb310250a3412507c7b624ed793924e404177
Sreevasan Siva <s.sreevasan@gmail.com>
2025-03-11 13:58:11 -0500

    committed the devlog.md

--- devlog.md
+++ devlog.md
@@ -5,3 +5,9 @@ I will be doing this project in python, so I will utilize the subprocess module 
 
 1:37 PM, 3/6/2025
 I am looking through the files right now. I have understood how to encrypt and decrypt the program and I will setup those functions.
+
+7:51 PM, 3/9/2025
+So far, I understand how the project is supposed to run. Main file which makes subprocesses where one is the logger file and the other is with the encryption file. I haven't had any new thoughts since the last session. I plan to further finish the logger and then work on the driver file. My goal is to finish the logger, and then maybe start with the driver.
+
+1:40 PM, 3/11/2025
+I got a better understanding of the flow between the driver file to the encryption and logger. I need to rechange the scheme in driver and encryption to better send instructions from driver to those subprocesses. My goal is to setup the connection between the logger and the driver properly this work session.
--- mem.py
+++ mem.py
@@ -1,39 +1,17 @@
 #!/usr/bin/env python
-
 import sys
-from datetime import datetime
 
-filename = sys.argv[1]
-mode = sys.stdin.readline().rstrip()  
 memloc = 0
 
-with open(filename, 'a') as file:
-    
-    now = datetime.now()
-    formatted_time = now.strftime("%Y-%m-%d %H:%M")
-    file.write(f"{formatted_time} [START] Logging Started\n")
-    file.flush()
+mode = sys.stdin.readline().rstrip()
 
-    while mode != "halt":
-        
-        formatted_time = datetime.now().strftime("%Y-%m-%d %H:%M")
-        
-        if mode == "read":
-            
-            file.write(f"{formatted_time} [READ] {memloc}\n")
-            sys.stdout.flush()
-        elif mode == "write":
-            
-            memloc = int(sys.stdin.readline().rstrip())
-            file.write(f"{formatted_time} [WRITE] {memloc}\n")
-        
-        
-        mode = sys.stdin.readline().rstrip()
-
-    
-    formatted_time = datetime.now().strftime("%Y-%m-%d %H:%M")
-    file.write(f"{formatted_time} [QUIT] Logging stopped\n")
-    file.flush()
+while mode != "halt":
+    if mode == "read":
+        print(memloc)
+        sys.stdout.flush()
+    elif mode == "write":
+        memloc = int(sys.stdin.readline().rstrip())
+    mode = sys.stdin.readline().rstrip()
 
 
 
